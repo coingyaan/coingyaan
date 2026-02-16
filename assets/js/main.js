45a121e34f31255d15c8fe612b66140266a48920
Monesh K <62913922+coingyaan@users.noreply.github.com>
2026-02-16 21:22:14 +0530

    Add global share helpers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
+// ===== SHARING HELPERS =====
+
+function shareOnX(text, url) {
+  const shareUrl =
+    "https://twitter.com/intent/tweet?text=" +
+    encodeURIComponent(text + "\n" + url);
+  window.open(shareUrl, "_blank");
+}
+
+function shareOnTelegram(text, url) {
+  const shareUrl =
+    "https://t.me/share/url?url=" +
+    encodeURIComponent(url) +
+    "&text=" +
+    encodeURIComponent(text);
+  window.open(shareUrl, "_blank");
+}
 
