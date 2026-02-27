ae341159215dd252b4a89e819299d1b5105d0602
Monesh K <62913922+coingyaan@users.noreply.github.com>
2026-02-27 23:20:05 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,1 @@
-// ===== SHARING HELPERS =====
-
-function shareOnX(text, url) {
-  const shareUrl =
-    "https://twitter.com/intent/tweet?text=" +
-    encodeURIComponent(text + "\n" + url);
-  window.open(shareUrl, "_blank");
-}
-
-function shareOnTelegram(text, url) {
-  const shareUrl =
-    "https://t.me/share/url?url=" +
-    encodeURIComponent(url) +
-    "&text=" +
-    encodeURIComponent(text);
-  window.open(shareUrl, "_blank");
-}
-
+// main.js
