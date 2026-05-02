207086660949161d327462e0207bdce50ec80e54
Monesh K <62913922+coingyaan@users.noreply.github.com>
2026-05-02 15:40:45 +0530

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -57,7 +57,7 @@
       border-radius: 10px;
       padding: 6px;
       min-width: 210px;
-      z-index: 999;
+      z-index: 999999;
       box-shadow: 0 8px 24px rgba(0,0,0,0.4);
     }
     .nav-dropdown-menu.open { display: block; }
